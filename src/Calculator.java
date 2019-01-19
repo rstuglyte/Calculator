53448a820f1f38c55414bbe885567cd0c18abd96
Rasa Stuglyte <rastug@yahoo.com>
2019-01-19 17:09:14 -0500

    local divide

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,5 +9,10 @@ public class Calculator {
      public static double multiply(double num1, double num2){
          return num1*num2;
      }
+     public static double divide(double n1, double n2){
+        double result = n1/n2;
+        return result;
+     }
+     // this a com
 
 }
